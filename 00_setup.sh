3a95c0ac114e6fa860238c10432297e5e203b3e1
Joe Manthey <jdmanthey@gmail.com>
2024-04-08 11:21:04 -0500

    Create 00_setup.sh

---
+++ 00_setup.sh
@@ -0,0 +1,21 @@
+# move to working directory
+
+# index reference genome 
+
+# make directories for organization during analyses
+mkdir 00_fastq
+mkdir 01_cleaned
+mkdir 01_bam_files
+mkdir 02_vcf
+mkdir 03_vcf
+mkdir 04_vcf
+mkdir 05_filtered_vcf
+mkdir 06_structure
+mkdir 07_phylogeny
+mkdir 08_stats
+mkdir 09_relatedness
+mkdir 10_popsize
+mkdir 11_eems
+mkdir 20_align_script
+mkdir 21_genotype_script
+mkdir 22_filter_script
