d111cf540b39f4022bd0104408de2e191b54d31f
Joe Manthey <jdmanthey@gmail.com>
2024-04-20 17:42:37 -0500

    Create 04_initial_filter.sh

---
+++ 04_initial_filter.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=filter
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=nocona
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+
+source activate bcftools
+
+# define main working directory
+workdir=/lustre/scratch/jmanthey/10_bighorn_rad
+
+# filter based on missing data for each of the subsets of data
+vcftools --vcf ${workdir}/04_vcf/total.vcf --max-missing 0.9 \
+--max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all --out ${workdir}/05_filtered_vcf/total
+
