b05a8809e54aa90a948665980b7b3bb30e3f8bc4
Joe Manthey <jdmanthey@gmail.com>
2024-04-20 18:31:35 -0500

    Update 04_initial_filter.sh

--- 04_initial_filter.sh
+++ 04_initial_filter.sh
@@ -12,7 +12,7 @@ source activate bcftools
 # define main working directory
 workdir=/lustre/scratch/jmanthey/10_bighorn_rad
 
-# filter based on missing data for each of the subsets of data
-vcftools --vcf ${workdir}/04_vcf/total.vcf --max-missing 0.9 \
+# filter based on missing data (max = 10 individuals or 0.042 proportion)
+vcftools --vcf ${workdir}/04_vcf/total.vcf --max-missing 0.958 \
 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all --out ${workdir}/05_filtered_vcf/total
 
