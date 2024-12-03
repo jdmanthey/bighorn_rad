1a1bd29db05a7ecd47fc8d9f50b082157f20d121
Joe Manthey <jdmanthey@gmail.com>
2024-12-03 11:54:08 -0600

    Update 01_align_genotype.sh

--- 01_align.sh
+++ 01_align.sh
@@ -1,53 +1,104 @@
 #!/bin/bash
 #SBATCH --chdir=./
-#SBATCH --job-name=bam
-#SBATCH --partition quanah
-#SBATCH --nodes=1 --ntasks=4
+#SBATCH --job-name=genotype
+#SBATCH --partition nocona
+#SBATCH --nodes=1 --ntasks=8
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=4G
 #SBATCH --array=1-244
 
-module load intel java bwa singularity
+source activate bcftools
 
-export SINGULARITY_CACHEDIR="/lustre/work/jmanthey/singularity-cachedir"
+threads=8
 
 # define main working directory
-workdir=/lustre/scratch/jmanthey/10_bighorn_rad
+workdir=/lustre/scratch/jmanthey/07_bighorn_rad
 
+# base name of fastq files, intermediate files, and output files
 basename_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/basenames.txt | tail -n1 )
 
 # define the reference genome
-refgenome=/home/jmanthey/references/GCF_016772045.2_ARS-UI_Ramb_v3.0_genomic.fna
+refgenome=/home/jmanthey/references/GCA_042477335.2_ARS-UI_OviCan_v2_genomic.fna 
 
-# run bbduk to clean and filter
-/lustre/work/jmanthey/bbmap/bbduk.sh in1=${workdir}/00_fastq/${basename_array}_R1.fastq.gz in2=${workdir}/00_fastq/${basename_array}_R2.fastq.gz out1=${workdir}/01_cleaned/${basename_array}_R1.fastq.gz out2=${workdir}/01_cleaned/${basename_array}_R2.fastq.gz minlen=50 ftl=5 qtrim=rl trimq=10 ktrim=r k=25 mink=7 ref=/lustre/work/jmanthey/bbmap/resources/adapters.fa hdist=1 tbo tpe
+# run bbduk
+/lustre/work/jmanthey/bbmap/bbduk.sh \
+in1=${workdir}/00_fastq/${basename_array}_R1.fastq.gz \
+in2=${workdir}/00_fastq/${basename_array}_R2.fastq.gz \
+out1=${workdir}/01_cleaned/${basename_array}_R1.fastq.gz \
+out2=${workdir}/01_cleaned/${basename_array}_R2.fastq.gz \
+minlen=50 ftl=10 qtrim=rl trimq=10 ktrim=r k=25 mink=7 \
+ref=/lustre/work/jmanthey/bbmap/resources/adapters.fa hdist=1 tbo tpe
 
 # run bwa mem
-bwa mem -t 4 ${refgenome} ${workdir}/01_cleaned/${basename_array}_R1.fastq.gz ${workdir}/01_cleaned/${basename_array}_R2.fastq.gz > ${workdir}/01_bam_files/${basename_array}.sam
+bwa-mem2 mem -t ${threads} ${refgenome} \
+${workdir}/01_cleaned/${basename_array}_R1.fastq.gz \
+${workdir}/01_cleaned/${basename_array}_R2.fastq.gz > \
+${workdir}/01_bam_files/${basename_array}.sam
 
-# convert sam to bam
-samtools view -b -S -o ${workdir}/01_bam_files/${basename_array}.bam ${workdir}/01_bam_files/${basename_array}.sam
+# filter for mapped reads
+samtools view -b -f 2 -@ ${threads} \
+-o ${workdir}/01_bam_files/${basename_array}.bam \
+${workdir}/01_bam_files/${basename_array}.sam
 
 # remove sam
 rm ${workdir}/01_bam_files/${basename_array}.sam
 
 # clean up the bam file
-singularity exec $SINGULARITY_CACHEDIR/gatk_4.2.3.0.sif gatk CleanSam -I ${workdir}/01_bam_files/${basename_array}.bam -O ${workdir}/01_bam_files/${basename_array}_cleaned.bam
+java -jar /home/jmanthey/picard.jar CleanSam \
+I=${workdir}/01_bam_files/${basename_array}.bam \
+O=${workdir}/01_bam_files/${basename_array}_cleaned.bam
 
 # remove the raw bam
 rm ${workdir}/01_bam_files/${basename_array}.bam
 
 # sort the cleaned bam file
-singularity exec $SINGULARITY_CACHEDIR/gatk_4.2.3.0.sif gatk SortSam -I ${workdir}/01_bam_files/${basename_array}_cleaned.bam -O ${workdir}/01_bam_files/${basename_array}_cleaned_sorted.bam --SORT_ORDER coordinate
+java -jar /home/jmanthey/picard.jar SortSam \
+I=${workdir}/01_bam_files/${basename_array}_cleaned.bam \
+O=${workdir}/01_bam_files/${basename_array}_cleaned_sorted.bam SORT_ORDER=coordinate
 
 # remove the cleaned bam file
 rm ${workdir}/01_bam_files/${basename_array}_cleaned.bam
 
 # add read groups to sorted and cleaned bam file
-singularity exec $SINGULARITY_CACHEDIR/gatk_4.2.3.0.sif gatk AddOrReplaceReadGroups -I ${workdir}/01_bam_files/${basename_array}_cleaned_sorted.bam -O ${workdir}/01_bam_files/${basename_array}_final.bam --RGLB 1 --RGPL illumina --RGPU unit1 --RGSM ${basename_array}
+java -jar /home/jmanthey/picard.jar AddOrReplaceReadGroups \
+I=${workdir}/01_bam_files/${basename_array}_cleaned_sorted.bam \
+O=${workdir}/01_bam_files/${basename_array}_final.bam \
+RGLB=1 RGPL=illumina RGPU=unit1 RGSM=${basename_array}
 
-# remove cleaned and sorted bam file
+# remove the cleaned bam file
 rm ${workdir}/01_bam_files/${basename_array}_cleaned_sorted.bam
 
 # index the final bam file
 samtools index ${workdir}/01_bam_files/${basename_array}_final.bam
+
+# run bcftools to genotype
+bcftools mpileup --skip-indels -C 0 -d 200 --min-MQ 10 --threads ${threads} \
+-f ${refgenome} ${workdir}/01_bam_files/${basename_array}_final.bam | \
+bcftools call -m --threads ${threads} -o ${workdir}/02_vcf/${basename_array}.vcf
+
+# bgzip
+bgzip ${workdir}/02_vcf/${basename_array}.vcf
+
+#tabix
+tabix ${workdir}/02_vcf/${basename_array}.vcf.gz
+
+# filter individual vcf files
+bcftools view -i 'MIN(DP)>7' ${workdir}/02_vcf/${basename_array}.vcf.gz > \
+${workdir}/03_vcf/${basename_array}.vcf
+
+# bgzip
+bgzip ${workdir}/03_vcf/${basename_array}.vcf
+
+#tabix
+tabix ${workdir}/03_vcf/${basename_array}.vcf.gz
+
+# alignment stats
+echo ${basename_array} > ${basename_array}.stats
+
+# samtools depth sum of aligned sites
+echo "samtools depth sum of aligned sites" >> ${basename_array}.stats
+samtools depth  ${workdir}/01_bam_files/${basename_array}_final.bam  |  awk '{sum+=$3} END { print "Sum = ",sum}' >> ${basename_array}.stats
+
+# number of genotyped sites passing minimum depth filter
+echo "sites genotyped" >> ${basename_array}.stats
+gzip -cd ${workdir}/03_vcf/${basename_array}.vcf.gz | grep -v "^#" | wc -l >> ${basename_array}.stats
